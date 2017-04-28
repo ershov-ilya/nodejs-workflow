8a727bc8fd6a80ddde734eaad57545f9aaff78c9
ILYA ERSHOV <ershov.ilya@gmail.com>
2017-04-28 12:33:30 +0300

    set of async functions

---
+++ index.js
@@ -0,0 +1,29 @@
+﻿let async = require('async')
+
+asyncStart(1);
+asyncStart(15);
+asyncStart(2);
+asyncStart(13);
+asyncStart(3);
+asyncStart(5);
+asyncStart(12);
+asyncStart(6);
+asyncStart(7);
+asyncStart(10);
+asyncStart(8);
+asyncStart(4);
+asyncStart(9);
+asyncStart(11);
+asyncStart(14);
+
+
+
+function asyncStart(num){
+    let promise = new Promise((resolve, reject) => {
+    setTimeout(function(){
+        let message='Process '+num+' done.'
+        console.log(message)
+        resolve({code: 200, message: message})
+    },num*300)
+    });
+}
