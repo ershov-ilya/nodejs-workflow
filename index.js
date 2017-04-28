195350ae4802fc28c95eeb2d8a497842f5af81aa
ILYA ERSHOV <ershov.ilya@gmail.com>
2017-04-28 13:39:54 +0300

    Разные примеры асинхронного выполнения

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 ﻿let async = require('async')
 
+
+
+function asyncStart(num, callback){
+    console.log('\x1b[36m%s\x1b[0m', 'Process '+num+' start.')
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(function(){
+            let message='Process '+num+' done.'
+            //console.log(message)
+            resolve({code: 200, num:num, message: message})
+        },num*100)
+    });
+    
+    promise.then(
+        result => {
+            console.log('\x1b[33m%s\x1b[0m', result.message)
+            if(typeof callback != 'undefined') callback(result)
+        },
+        error => {
+            console.error(error)
+        }
+    );
+}
+
+// Параллельный запуск
+/*
 asyncStart(1);
 asyncStart(15);
 asyncStart(2);
@@ -15,15 +40,119 @@ asyncStart(4);
 asyncStart(9);
 asyncStart(11);
 asyncStart(14);
+*/
+
+/*
+// Запуск параллельно
+async.parallel([
+    function(callback) { asyncStart(1,  callback) },
+    function(callback) { asyncStart(15,  callback) },
+    function(callback) { asyncStart(2,  callback) },
+    function(callback) { asyncStart(13,  callback) },
+    function(callback) { asyncStart(3,  callback) },
+    function(callback) { asyncStart(5,  callback) },
+    function(callback) { asyncStart(12,  callback) },
+    function(callback) { asyncStart(6,  callback) },
+    function(callback) { asyncStart(7,  callback) },
+    function(callback) { asyncStart(10,  callback) },
+    function(callback) { asyncStart(8,  callback) },
+    function(callback) { asyncStart(4,  callback) },
+    function(callback) { asyncStart(9,  callback) },
+    function(callback) { asyncStart(11,  callback) },
+    function(callback) { asyncStart(14,  callback) }
+]);
+/**/
+
+/*
+// Запуск в N параллельных потоков
+async.parallelLimit([
+    function(callback) { asyncStart(1,  callback) },
+    function(callback) { asyncStart(15,  callback) },
+    function(callback) { asyncStart(2,  callback) },
+    function(callback) { asyncStart(13,  callback) },
+    function(callback) { asyncStart(3,  callback) },
+    function(callback) { asyncStart(5,  callback) },
+    function(callback) { asyncStart(12,  callback) },
+    function(callback) { asyncStart(6,  callback) },
+    function(callback) { asyncStart(7,  callback) },
+    function(callback) { asyncStart(10,  callback) },
+    function(callback) { asyncStart(8,  callback) },
+    function(callback) { asyncStart(4,  callback) },
+    function(callback) { asyncStart(9,  callback) },
+    function(callback) { asyncStart(11,  callback) },
+    function(callback) { asyncStart(14,  callback) }
+], 3);
+/**/
 
 
+/*
+// Запуск последовательно
+async.series([
+    function(callback) { asyncStart(1,  callback) },
+    function(callback) { asyncStart(15,  callback) },
+    function(callback) { asyncStart(2,  callback) },
+    function(callback) { asyncStart(13,  callback) },
+    function(callback) { asyncStart(3,  callback) },
+    function(callback) { asyncStart(5,  callback) },
+    function(callback) { asyncStart(12,  callback) },
+    function(callback) { asyncStart(6,  callback) },
+    function(callback) { asyncStart(7,  callback) },
+    function(callback) { asyncStart(10,  callback) },
+    function(callback) { asyncStart(8,  callback) },
+    function(callback) { asyncStart(4,  callback) },
+    function(callback) { asyncStart(9,  callback) },
+    function(callback) { asyncStart(11,  callback) },
+    function(callback) { asyncStart(14,  callback) }
+]);
+// передает все результаты в окончательный обратный вызов
+// for discrete tasks
+/**/
+// Не вижу разницы async.series <-> async.waterfall
 
-function asyncStart(num){
-    let promise = new Promise((resolve, reject) => {
-    setTimeout(function(){
-        let message='Process '+num+' done.'
-        console.log(message)
-        resolve({code: 200, message: message})
-    },num*300)
-    });
-}
+/*
+// Запуск водопадом
+async.waterfall([
+    function(callback) { asyncStart(1,  callback) },
+    function(callback) { asyncStart(15,  callback) },
+    function(callback) { asyncStart(2,  callback) },
+    function(callback) { asyncStart(13,  callback) },
+    function(callback) { asyncStart(3,  callback) },
+    function(callback) { asyncStart(5,  callback) },
+    function(callback) { asyncStart(12,  callback) },
+    function(callback) { asyncStart(6,  callback) },
+    function(callback) { asyncStart(7,  callback) },
+    function(callback) { asyncStart(10,  callback) },
+    function(callback) { asyncStart(8,  callback) },
+    function(callback) { asyncStart(4,  callback) },
+    function(callback) { asyncStart(9,  callback) },
+    function(callback) { asyncStart(11,  callback) },
+    function(callback) { asyncStart(14,  callback) }
+]);
+// каждая функция передаёт свои результаты на следующую функцию
+// for a data pipeline
+/**/
+
+
+// Гонка
+async.race([
+    function(callback) { asyncStart(1,  callback) },
+    function(callback) { asyncStart(15,  callback) },
+    function(callback) { asyncStart(2,  callback) },
+    function(callback) { asyncStart(13,  callback) },
+    function(callback) { asyncStart(3,  callback) },
+    function(callback) { asyncStart(5,  callback) },
+    function(callback) { asyncStart(12,  callback) },
+    function(callback) { asyncStart(6,  callback) },
+    function(callback) { asyncStart(7,  callback) },
+    function(callback) { asyncStart(10,  callback) },
+    function(callback) { asyncStart(8,  callback) },
+    function(callback) { asyncStart(4,  callback) },
+    function(callback) { asyncStart(9,  callback) },
+    function(callback) { asyncStart(11,  callback) },
+    function(callback) { asyncStart(14,  callback) }
+], function(data){
+    console.log('Final callback: ', data)
+});
+/**/
+
+
